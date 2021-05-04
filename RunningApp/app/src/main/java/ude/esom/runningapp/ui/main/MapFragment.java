188b21e4a804e6d96535a5c7c8e8e7b4ac3736f2
Albright <albrightcm@ad.msoe.edu>
2021-05-04 15:57:41 -0500

    went ham on tracking a run, now you get pace, time, and distance

--- RunningApp/app/src/main/java/ude/esom/runningapp/ui/main/MapFragment.java
+++ RunningApp/app/src/main/java/ude/esom/runningapp/ui/main/MapFragment.java
@@ -1,29 +1,90 @@
 package ude.esom.runningapp.ui.main;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
+import java.lang.ref.WeakReference;
+import java.text.DecimalFormat;
+
 import ude.esom.runningapp.R;
 
-public class MapFragment extends Fragment {
+import static android.graphics.Color.rgb;
+import static android.os.Looper.getMainLooper;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
 
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private LocationEngine locationEngine;
+    private long DEFAULT_INTERVAL_IN_MILLISECONDS = 2000L;
+    private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
+    private MainActivityLocationCallback callback = new MainActivityLocationCallback(this);
+    private static DecimalFormat df = new DecimalFormat("##.##");
+    private TextView paceText;
+    private TextView distanceText;
+    private TextView timeText;
+    private long startTime = 0;
+    private double distance = 0;
+    private boolean isRunning = false;
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int paceMinutes = (int) ((seconds/distance)/60);
+            int paceSeconds = (int) ((seconds/distance) % 60);
+            int hours = seconds / 3600;
+            int minutes = seconds % 3600 / 60;
+            seconds = seconds % 60;
+            timeText.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+            distanceText.setText(df.format(distance));
+            paceText.setText(String.format("%02d:%02d", paceMinutes, paceSeconds));
+            timerHandler.postDelayed(this, 1000);
+        }
+    };
+
+    @SuppressLint("MissingPermission")
+    private void initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(getContext());
+
+        LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+        locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+        locationEngine.getLastLocation(callback);
+    }
 
     public static MapFragment newInstance(int index) {
         MapFragment fragment = new MapFragment();
@@ -50,24 +111,114 @@ public class MapFragment extends Fragment {
         Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token));
 
         View root = inflater.inflate(R.layout.map_fragment, container, false);
-
         mapView = (MapView) root.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
+
+
+        paceText = root.findViewById(R.id.text_pace);
+        distanceText = root.findViewById(R.id.text_miles);
+        timeText = root.findViewById(R.id.text_time);
+        Button run = root.findViewById(R.id.button_run);
+
+        run.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+            public void onClick(View v) {
+                isRunning = !isRunning;
+                if (isRunning){
+                    startTime = System.currentTimeMillis();
+                    distance = 0;
+                    run.setBackgroundColor(rgb(200, 0, 0));
+                    run.setText("Stop Run");
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    LocationComponent locationComponent = mapboxMap.getLocationComponent();
+                    locationComponent.setCameraMode(CameraMode.TRACKING);
+                } else{
+                    timerHandler.removeCallbacks(timerRunnable);
+                    run.setBackgroundColor(rgb(0, 255, 0));
+                    run.setText("Start Run");
 
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-
-                    }
-                });
-
+                }
             }
         });
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
+                    LocationComponent locationComponent = mapboxMap.getLocationComponent();
+                    LocationComponentActivationOptions locationComponentActivationOptions = LocationComponentActivationOptions.builder(getContext(), style).useDefaultLocationEngine(false).build();
+                    locationComponent.activateLocationComponent(locationComponentActivationOptions);
+                    locationComponent.setLocationComponentEnabled(true);
+                    locationComponent.setCameraMode(CameraMode.TRACKING);
+                    locationComponent.setRenderMode(RenderMode.COMPASS);
+                    locationEngine = LocationEngineProvider.getBestLocationEngine(getContext());
+                    LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS).setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY).setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+                    locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+                    locationEngine.getLastLocation(callback);
+                }
+            }
+        });
+    }
+
+    private static class MainActivityLocationCallback implements LocationEngineCallback<LocationEngineResult> {
+        private final WeakReference<MapFragment> activityWeakReference;
+
+        MainActivityLocationCallback(MapFragment activity) {
+            this.activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onSuccess(LocationEngineResult result) {
+            MapFragment activity = activityWeakReference.get();
+            if (activity != null) {
+                Location location = result.getLastLocation();
+                if (location == null) {
+                    return;
+                }
+                if (activity.mapboxMap != null && result.getLastLocation() != null) {
+                    double oldLat, newLat, oldLong, newLong;
+                    if(activity.mapboxMap.getLocationComponent().getLastKnownLocation() == null){
+                        oldLat = result.getLastLocation().getLatitude();
+                        oldLong = result.getLastLocation().getLongitude();
+                        System.out.print("1");
+                    } else {
+                        oldLat = activity.mapboxMap.getLocationComponent().getLastKnownLocation().getLatitude();
+                        oldLong = activity.mapboxMap.getLocationComponent().getLastKnownLocation().getLongitude();
+                    }
+
+                    activity.mapboxMap.getLocationComponent().forceLocationUpdate(result.getLastLocation());
+
+                    newLat = result.getLastLocation().getLatitude();
+                    newLong = result.getLastLocation().getLongitude();
+
+                    double dLat = (newLat - oldLat) *
+                            Math.PI / 180.0;
+                    double dLon = (newLong - oldLong) *
+                            Math.PI / 180.0;
+                    oldLat = (oldLat) * Math.PI / 180.0;
+                    newLat = (newLat) * Math.PI / 180.0;
+                    double a = Math.pow(Math.sin(dLat / 2), 2) +
+                            Math. pow(Math.sin(dLon / 2), 2) *
+                                    Math.cos(oldLat) * Math.cos(newLat);
+                    double rad = 6371;
+                    double c = 2 * Math.asin(Math.sqrt(a));
+                    activity.distance += rad * c;
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+
+        }
     }
 
     @Override
@@ -86,6 +237,7 @@ public class MapFragment extends Fragment {
     public void onPause() {
         super.onPause();
         mapView.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
     }
 
     @Override
@@ -109,6 +261,10 @@ public class MapFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        mapView.onDestroy();
+        if (locationEngine != null) {
+            locationEngine.removeLocationUpdates(callback);
+        }
         mapView.onDestroy();
     }
 }
